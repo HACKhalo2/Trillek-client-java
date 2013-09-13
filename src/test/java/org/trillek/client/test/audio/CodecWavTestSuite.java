c7e29ce29187d61e3d7d9a5dcb210a80ebf66b50
HACKhalo2 <hackhalo2@gmail.com>
2013-09-13 02:01:49 -0400

    I'm just not going to touch the audio for a while until I figure out what the fuck I'm going to use

---
+++ src/test/java/org/trillek/client/test/audio/CodecWavTestSuite.java
@@ -0,0 +1,24 @@
+package org.trillek.client.test.audio;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CodecWavTestSuite {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
